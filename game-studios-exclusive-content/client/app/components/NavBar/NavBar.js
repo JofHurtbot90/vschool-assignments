10b9c0cc5fb55f7e68f149fd8514bd4660ff46c8
Joshua Hurtado <josha7xnightreaper@gmail.com>
2017-12-16 01:05:41 -0500

    update

---
+++ game-studios-exclusive-content/client/app/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+export default NavBar(props){
+  return(
+    <nav className='navbar'>
+      <Link></Link>
+      <Link></Link>
+      <Link></Link>
+    </nav>
+  )
+}
